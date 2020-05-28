23b1bd573636825a8f427c805a1041212e3f9a5f
blattersturm <peachypies@protonmail.ch>
2020-05-28 20:30:42 +0200

    remove temp file logging

--- clcache/__main__.py
+++ clcache/__main__.py
@@ -1091,13 +1091,11 @@ def findCompilerBinary():
                 return path
     return None
 
-of = open('X:/clcache.log', 'a')
-
 def printTraceStatement(msg: str) -> None:
     if "CLCACHE_LOG" in os.environ:
         scriptDir = os.path.realpath(os.path.dirname(sys.argv[0]))
         with OUTPUT_LOCK:
-            print(os.path.join(scriptDir, "clcache.py") + " " + msg, file = of)
+            print(os.path.join(scriptDir, "clcache.py") + " " + msg)
 
 
 class CommandLineTokenizer:
